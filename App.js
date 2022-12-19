37c992e17fd28f55970a8d8cfb268ab88846b628
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-12-19 15:48:35 -0500

    added packages i'll need, imported fonts, and imported logo

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {Text, View } from 'react-native';
 export default function App() {
   return (
     <View>
-      <Text>Blue Lotus Meditations</Text>
+      <Text>Blue Lotus Meditation</Text>
       <StatusBar style="auto" />
     </View>
   );
