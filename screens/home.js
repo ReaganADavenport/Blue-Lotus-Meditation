a6e5f2ab30fdad67c17f0e5ac375ed3742058d7e
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-12-19 16:16:45 -0500

    learned how to use images in react-native, applied styles to the blue-lotus-logo

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Text, View, Button, TouchableOpacity} from 'react-native';
+import { Text, View, Button, TouchableOpacity, Image} from 'react-native';
 import { globalStyles } from '../styles/global';
+
+import logo from '../assets/Blue-Lotus-logo.png'
 
 export default function Home(){
 
     return(
-        <View style={globalStyles.container}>
-            
+        <View style={globalStyles.homeContainer}>
+            <Text style={globalStyles.titleText}>Blue Lotus Mediation</Text>
+            <Image style={globalStyles.logoImage} source={logo}/>
+            <Button title='Get Started'/>
         </View>
     )
 }
--- styles/global.js
+++ styles/global.js
@@ -1,16 +1,24 @@
 import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
-    container: {
-        
-        backgroundColor: '#fff',
+    homeContainer: {
+        fontFamily: 'vesper-regular',
+        backgroundColor: '#226170',
         flex: 1,
         padding: 45,
     },
 
     titleText:{
+        fontFamily: 'vesper-bold',
         fontSize: 18,
         color: '#333',
+    },
+
+    logoImage:{
+        alignContent: 'center',
+        justifyContent: 'center',
+        height: 200,
+        width: 300,
     },
 
     paragraph: {
