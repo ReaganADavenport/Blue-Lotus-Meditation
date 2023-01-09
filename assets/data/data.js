4b9c8adeb64e58639f4f458f6971db19cced057f
Reagan Davenport <reagan.a.davenport@gmail.com>
2023-01-09 10:21:58 -0500

    created a data.js to house the title, image, and id of the meditations

---
+++ assets/data/data.js
@@ -0,0 +1,25 @@
+const mediations = [
+    {
+        title: "Sleep Meditation",
+        image: require("../images/sleep-logo.png"),
+        id: "1",
+    },
+
+    {
+        title: "Mindfulness Meditation",
+        image: require("../images/mindful-logo.png"),
+        id: "2",
+    },
+
+    {
+        title: "Relaxation Meditation",
+        image: require("../images/relax-logo.png"),
+        id: "3",
+    },
+
+    {
+        title: "Reflection Meditation",
+        image: require("../images/reflect-logo.png"),
+        id: "4",
+    }
+]
