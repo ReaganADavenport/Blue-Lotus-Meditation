f39fb16d9078cca0329bf0bf5941f17df8e49e25
Reagan Davenport <reagan.a.davenport@gmail.com>
2023-01-09 14:31:51 -0500

    added mp3s of meditation music, added urls to data.js

--- assets/data/data.js
+++ assets/data/data.js
@@ -2,24 +2,28 @@ const mediations = [
     {
         title: "Sleep Meditation",
         image: require("../images/sleep-logo.png"),
+        url: require('../audio/sleep.mp3'),
         id: "1",
     },
 
     {
         title: "Mindfulness Meditation",
         image: require("../images/mindful-logo.png"),
+        url: require('../audio/mindfulness.mp3'),
         id: "2",
     },
 
     {
         title: "Relaxation Meditation",
         image: require("../images/relax-logo.png"),
+        url: require('../audio/relax.mp3'),
         id: "3",
     },
 
     {
         title: "Reflection Meditation",
         image: require("../images/reflect-logo.png"),
+        url: require('../audio/reflection.mp3'),
         id: "4",
     }
 ]
